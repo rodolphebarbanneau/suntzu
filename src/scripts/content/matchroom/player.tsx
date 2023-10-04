0c6c8a6347eb687eb0c58830fe45399f4033a56d
rodolphebarbanneau <46493454+rodolphebarbanneau@users.noreply.github.com>
2023-10-04 14:22:42 +0200

    🗑️ Clean player feature

--- src/scripts/content/matchroom/player.tsx
+++ src/scripts/content/matchroom/player.tsx
@@ -1,22 +1,7 @@
-import { default as ReactShadowRoot } from 'react-shadow';
-
 import type { Matchroom } from 'src/shared/core';
 import { Feature } from 'src/shared/core';
-import { getColorScale } from 'src/shared/helpers';
-
-import { Metrics } from 'src/app/components/metrics';
-import { Tooltip } from 'src/app/components/tooltip';
-
-import stylesheetLink from 'src/app/components/link.module.scss?inline';
-import stylesheetMetrics from 'src/app/components/metrics.module.scss?inline';
-import stylesheetTooltip from 'src/app/components/tooltip.module.scss?inline';
-
-import stylesheet from './player.module.scss?inline';
-import styles from './player.module.scss';
 
 /* Player feature */
 export const PlayerFeature = (matchroom: Matchroom) => new Feature('player',
-  (feature) => {
-    console.log('todo');
-  },
+  (feature) => {},
 );
